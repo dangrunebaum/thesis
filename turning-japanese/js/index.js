19c5bec70296ea04be3f5ef3c543684be79379e8
Dan Grunebaum <42687809+dangrunebaum@users.noreply.github.com>
2020-04-12 12:59:48 -0400

    test audio with full site path name

--- turning-japanese/js/index.js
+++ turning-japanese/js/index.js
@@ -192,7 +192,8 @@ d3.csv("data/100-frequent-words.csv", function (data) {
         // console.log(item.Loanword);
         var pronunciation = document.getElementById('audioElement');
         pronunciation.setAttribute('src', 
-        `https://dangrunebaum.github.io/data/audio/typhoon--_us_1.mp3`
+//         `https://dangrunebaum.github.io/data/audio/typhoon--_us_1.mp3`
+                                   `https://dangrunebaum.github.io/turning-japanese/data/audio/typhoon--_us_1.mp3`
         // `/data/audio/${item.Loanword}--_us_1.mp3`
         );
         pronunciation.play()
